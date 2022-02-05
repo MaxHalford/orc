1c94bf0784ff9171b1f2fa03bc0c19685d6c9fd5
Max Halford <maxhalford25@gmail.com>
2022-02-05 18:51:50 +0100

    don't include pattern

--- orc/near_match.py
+++ orc/near_match.py
@@ -7,17 +7,15 @@ import orc
 @dataclass
 class NearMatch:
     specimen: str
-    pattern: str
     candidate: str
     start: int
     end: int
     edits: orc.Edits
 
     @classmethod
-    def from_regex(cls, match: regex.Match, specimen: str, pattern: str):
+    def from_regex(cls, match: regex.Match, specimen: str):
         return cls(
             specimen=specimen,
-            pattern=pattern,
             candidate=match.group(),
             start=match.start(),
             end=match.end(),
